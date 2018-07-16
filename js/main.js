80a9c83763b3cdd7b4750ec9ad30b6c4fb883260
Davi Barreto <davivbarreto@gmail.com>
2018-07-16 15:47:35 +0300

    Improvements for a11y in the home page.

--- js/main.js
+++ js/main.js
@@ -189,6 +189,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.setAttribute('aria-label', 'for more information');
   li.append(more);
 
   return li;
